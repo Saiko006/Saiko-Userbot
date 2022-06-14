6437fc8dcc43c19cf6d87f57c230ad87c54f38a6
Saiko006 <107464573+Saiko006@users.noreply.github.com>
2022-06-14 18:44:13 +0700

    Update string_session.py

--- string_session.py
+++ string_session.py
@@ -19,7 +19,7 @@ API_HASH = input("Enter API_HASH here: ")
 with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
     print("Check Telegram Save Message Mu Untuk Copy STRING_SESSION ")
     session_string = client.session.save()
-    saved_messages_template = """😼 Thank u for Support Tonic-Userbot | Grup @PrimeSupportGroup
+    saved_messages_template = """😼 Thank u for Support Saiko-Userbot | Grup @SaikoSupport
 
 <code>STRING_SESSION</code>: <code>{}</code>
 
