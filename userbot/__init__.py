e6e43039021599b22a0fae3974c91157117ddd8d
Saiko006 <107464573+Saiko006@users.noreply.github.com>
2022-06-14 19:20:33 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -809,7 +809,7 @@ Voice chat group menu untuk {owner}
 """)
                 await event.edit(
                     text,
-                    file=saikologo,
+                    file=tonilogo,
                     link_preview=True,
                     buttons=[Button.inline("ʙᴀᴄᴋ", data="saiko_inline")])
             else:
